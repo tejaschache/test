f5321388bdd70fa28420e45715e97a620208d23c
tejaschache <tejaschache3@gmail.com>
2021-09-22 16:39:49 +0530

    first commit

---
+++ src/main/java/com/example/emailscheduler/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.emailscheduler;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EmailSchedulerApplication.class);
+	}
+
+}
